753288abb8c0725a4a06185724214aeb85bcd65c
EricSpencer00 <espencer2@luc.edu>
2026-09-20 03:27:30 -0500

    Add residual relation setting debt lane

---
+++ tests/test_residual_relation_setting_20260920.py
@@ -0,0 +1,5 @@
+from residual_relation_setting_20260920 import audit,run
+def test_two_slot_debt():
+ r=run(); assert r['stats']['rendered_candidates']>0; assert r['stats']['fresh_exact_gt38']==0
+ assert all(x['provenance']['debt_carried_across_two_slots'] for x in r['rendered_candidates'])
+def test_audit(): assert audit('A man, a plan, a canal: Panama!')['exact']

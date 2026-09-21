f61e8eb0df2b8626fd1b58c3e8e43879e30ae678
EricSpencer00 <espencer2@luc.edu>
2026-09-21 12:09:00 -0500

    Add six-event bridge cycle topology

---
+++ test_six_event_bridge_cycle_20260921.py
@@ -0,0 +1,4 @@
+from six_event_bridge_cycle_20260921 import support
+def test_endpoint_pairing():
+ assert support(['ab.','x.','x.','x.','x.','ba.'])[0]
+ assert not support(['ab.','x.','x.','x.','x.','ca.'])[0]

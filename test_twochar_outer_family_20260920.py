dbc9d5392ac101f6f335c845b2cccaba219e48e1
EricSpencer00 <espencer2@luc.edu>
2026-09-20 19:58:44 -0500

    Add two-character outer family experiment

---
+++ test_twochar_outer_family_20260920.py
@@ -0,0 +1,5 @@
+from twochar_outer_family_20260920 import run
+def test_twochar_family_is_filtered_and_audited():
+ d=run(); assert d['stats']['possible_pairs']>d['stats']['compatible_pairs']>0
+ assert len(d['diagnostic_controls'])==d['stats']['compatible_pairs']
+ assert all(r['pointer_audit']['independent_exact'] is False for r in d['diagnostic_controls'])
